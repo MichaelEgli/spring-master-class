76af8d6d59b7bb5a07aa82b7ca3e3140fcec25e1
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2022-12-26 14:42:25 +0100

    Add files via upload
    understandind tight and loosely coupling

---
+++ sping-in-5-steps-backup/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+
+    private SortAlgorithm sortAlgorithm;
+
+    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+        super();
+        this.sortAlgorithm = sortAlgorithm;
+    }
+
+    public int binarySearch(int[] numbers, int numberToSearchFor) {
+
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
+
+        // Search the array
+
+        return 3;
+    }
+}
