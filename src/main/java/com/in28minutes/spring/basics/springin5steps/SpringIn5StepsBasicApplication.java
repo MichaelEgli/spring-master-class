77f878741072229d26e62f77ef4cd67409d11796
MichaelEgli <michaelegli1879@gmail.com>
2022-12-30 14:23:44 +0100

    036 refactor to start -scope of beans tutorials-

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,11 +1,12 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	// What are the beans? => @Component (BinarySearchImpl, BubbleSortAlgorithm)
 	// What are the dependencies of a bean? => @Autowired (Interface sortAlgorithm)
@@ -14,10 +15,12 @@ public class SpringIn5StepsApplication {
 	public static void main(String[] args) {
 		System.out.println("Egli");
 		// is now managed by Spring: BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
-		System.out.println("result:" + result);
+		System.out.println("binarySearch: " + binarySearch);
+		System.out.println("binarySearch1: " + binarySearch1);
 	}
 
 }
---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -0,0 +1,31 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import com.in28minutes.spring.basics.springin5steps.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringIn5StepsScopeApplication {
+
+	static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
+
+	public static void main(String[] args) {
+
+		System.out.println("Egli Scope");
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
+
+		PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+		PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+
+		LOGGER.info("{} person", personDAO);
+		LOGGER.info("{} person.getJdbcConnection", personDAO.getJdbcConnection());
+
+		LOGGER.info("{} person2", personDAO2);
+		LOGGER.info("{} person2.getJdbcConnection", personDAO2.getJdbcConnection());
+	}
+
+}
