1b5ffcde56c667588a374d676d42a09ebe0f86ca
MichaelEgli <michaelegli1879@gmail.com>
2023-01-02 14:17:03 +0100

    048 use @service and @repository instead of @component

--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/QuickSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/QuickSortAlgorithm.java
@@ -1,8 +1,9 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class QuickSortAlgorithm implements SortAlgorithm{
 
     public int[] sort(int[] numbers) {
