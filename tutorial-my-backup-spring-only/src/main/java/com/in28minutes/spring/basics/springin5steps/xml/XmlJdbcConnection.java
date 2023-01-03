de85aba545d5ca65547bd2ee9527ecc3731849ec
Michael Egli <73283077+MichaelEgli@users.noreply.github.com>
2023-01-03 11:24:05 +0100

    Delete tutorial-my-backup-spring-only directory

--- tutorial-my-backup-spring-only/src/main/java/com/in28minutes/spring/basics/springin5steps/xml/XmlJdbcConnection.java
+++
@@ -1,8 +0,0 @@
-package com.in28minutes.spring.basics.springin5steps.xml;
-
-public class XmlJdbcConnection {
-
-    public XmlJdbcConnection() {
-        System.out.println("XML JDBC Connection");
-    }
-}
