7d28c7b17adbf47decb2a0d72ed955ccb68bd7df
MichaelEgli <michaelegli1879@gmail.com>
2023-01-03 11:21:52 +0100

    060 using mockito annotations

---
+++ src/test/java/com/in28minutes/spring/mockito/SomeBusinessMockAnnotationsTest.java
@@ -0,0 +1,42 @@
+package com.in28minutes.spring.mockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SomeBusinessMockAnnotationsTest {
+
+    @Mock
+    DataService dataServiceMock;
+
+    @InjectMocks
+    SomeBusinessImpl businessImpl;
+
+    @Test
+    public void testFindTheGreatestFromAllData() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 24, 15, 3});
+        int result = businessImpl.findTheGreatesFromaAllData();
+        assertEquals(24, result);
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllData_ForOneValue() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 15 });
+        int result = businessImpl.findTheGreatesFromaAllData();
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllData_NoValues() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { });
+        int result = businessImpl.findTheGreatesFromaAllData();
+        assertEquals(Integer.MIN_VALUE, result);
+    }
+}
+
